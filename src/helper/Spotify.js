97c0d4b08059a3372ff14faac3caab3aef6cf4d9
Ruona45 <58301241+Ruona45@users.noreply.github.com>
2023-09-14 19:04:38 +0100

    removed scope

--- src/helper/Spotify.js
+++ src/helper/Spotify.js
@@ -14,22 +14,19 @@ function generateRandomString(length) {
   return text;
 }
 
-
 // Function to initate the Spotify login flow. Generates a code verifier, code challenge and a random state like in the API documentation
-function initateLogin(){
-    const codeVerifier = generateRandomString(128);
-    const codeChallenge = btoa(codeVerifier); // btoa is a js function for binary to ASCII
-    const state = generateRandomString(16); // Optional in the documentation but it provides protection against attacks
-    const scope
+function initateLogin() {
+  const codeVerifier = generateRandomString(128);
+  const codeChallenge = btoa(codeVerifier); // btoa is a js function for binary to ASCII
+  const state = generateRandomString(16); // Optional in the documentation but it provides protection against attacks
 
-
-    // Following the documentation the required params
-    const params = new URLSearchParams({
-        client_id: clientID,
-        response_type: 'code',
-        redirect_uri: redirectURI,
-        state: state,
-        code_challenge_method: 'S256',
-        code_challenge: codeChallenge
-    });
+  // Following the documentation the required params
+  const params = new URLSearchParams({
+    client_id: clientID,
+    response_type: "code",
+    redirect_uri: redirectURI,
+    state: state,
+    code_challenge_method: "S256",
+    code_challenge: codeChallenge,
+  });
 }
